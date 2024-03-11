960540cbf29d5d081240c90d710677714888c575
DemirelZ <zaferdemirelgsb@gmail.com>
2024-03-11 12:58:08 +0300

    project started and CarItem componend added

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import {ImageBackground, StatusBar, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import CarItem from './src/components/CarItem';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+    <CarItem/>
+
+      <StatusBar style='auto'/>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+  
+});
