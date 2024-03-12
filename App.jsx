d23f429a58945de28f8517a6846d0005f963aef7
DemirelZ <zaferdemirelgsb@gmail.com>
2024-03-12 13:30:21 +0300

    readme updated

--- App.jsx
+++ App.jsx
@@ -1,25 +1,15 @@
-import { StatusBar, StyleSheet, Text, View} from 'react-native';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import CarList from './src/components/CarsList';
 import Header from './src/components/Header';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-    <Header/>
+    <View>
+      <Header />
       <CarList />
-      <StatusBar style="auto" />
     </View>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
